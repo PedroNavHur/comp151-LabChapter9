4cf53e3df3fc8f9aa52dde08e11c7304b23152af
Pedro Navarrete <pnavarrete@harding.edu>
2018-02-19 10:41:00 -0600

    Exercise 2 Solved

--- Comp151Lab9/dynamic.cpp
+++ Comp151Lab9/dynamic.cpp
@@ -1,5 +1,6 @@
 // This program demonstrates the use of dynamic variables
-// PLACE YOUR NAME HERE
+// Pedro J. Navarrete
+// V 1.00
 
 #include <iostream>
 using namespace std;
@@ -9,50 +10,64 @@ const int MAXNAME = 10;
 int main()
 
 {
-   int pos;
-   char * name;
-   int * one;
-   int * two;
-   int * three;
-   int result;
+	int pos;
+	char * name;
+	int * one;
+	int * two;
+	int * three;
+	int result;
 
-   //  Fill in code to allocate the integer variable one here
-   //  Fill in code to allocate the integer variable two here
-   //  Fill in code to allocate the integer variable three here
-   //  Fill in code to allocate the character array pointed by name
+	//  Fill in code to allocate the integer variable one here
+	one = new int;
+	//  Fill in code to allocate the integer variable two here
+	two = new int;
+	//  Fill in code to allocate the integer variable three here
+	three = new int;
+	//  Fill in code to allocate the character array pointed by name
+	name = new char[10];
 
-   cout << "Enter your last name with exactly 10 characters." << endl;
-   cout << "If your name has < 10 characters, repeat last letter. " << endl
+	cout << "Enter your last name with exactly 10 characters." << endl;
+	cout << "If your name has < 10 characters, repeat last letter. " << endl
 		<< "Blanks at the end do not count." << endl;
-   
-   for ( pos = 0;  pos < MAXNAME;   pos++)
 
-	  cin >> // Fill in code to read a character into the name array
-             // WITHOUT USING a bracketed subscript
+	for (pos = 0; pos < MAXNAME; pos++)
 
-   cout << "Hi ";
-   for ( pos = 0;  pos < MAXNAME;  pos++)
+		cin >> *(name + pos); // Fill in code to read a character into the name array
+			  // WITHOUT USING a bracketed subscript
 
-      cout << // Fill in code to a print a character from the name array
-              // WITHOUT USING a bracketed subscript
-  
-   cout << endl << "Enter three integer numbers separated by blanks" << endl;
+	cout << "Hi ";
 
-   // Fill in code to input three numbers and store them in the
-   // dynamic variables pointed to by pointers one, two, and three.
-   // You are working only with pointer variables
+	for (pos = 0; pos < MAXNAME; pos++)
 
-   //echo print
-   cout << "The three numbers are " << endl;
+		cout << *(name + pos);// Fill in code to a print a character from the name array
+				// WITHOUT USING a bracketed subscript
 
-   // Fill in code to output those numbers
+	cout << endl << "Enter three integer numbers separated by blanks" << endl;
 
-   result = // Fill in code to calculate the sum of the three numbers
-   cout << "The sum of the three values is " << result << endl;
+	// Fill in code to input three numbers and store them in the
+	// dynamic variables pointed to by pointers one, two, and three.
+	// You are working only with pointer variables
+	cin >> *one;
+	cin >> *two;
+	cin >> *three;
 
-  // Fill in code to deallocate one, two, three and name
+	//echo print
+	cout << "The three numbers are ";
+
+	// Fill in code to output those numbers
+	cout << *one << " ";
+	cout << *two << " ";
+	cout << *three << endl;
+
+	result = *one + *two + *three; // Fill in code to calculate the sum of the three numbers
+		cout << "The sum of the three values is " << result << endl;
+
+	// Fill in code to deallocate one, two, three and name
+		delete one;
+		delete two;
+		delete three;
+		delete name;
 
 
-
-  return 0;
+	return 0;
 }
