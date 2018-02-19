698017f89abab0386a2ccae7f6264f3a5ab701cb
Pedro Navarrete <pnavarrete@harding.edu>
2018-02-19 10:24:22 -0600

    Exercise 1 Solved

--- Comp151Lab9/pointers.cpp
+++ Comp151Lab9/pointers.cpp
@@ -2,7 +2,8 @@
 // It finds the area of a rectangle given length and width
 // It prints the length and width in ascending order 
 
-// PLACE YOUR NAME HERE
+// Pedro J. Navarrete
+// V 0.00
 
 #include <iostream>
 using namespace std;
@@ -10,37 +11,38 @@ using namespace std;
 int main()
 {
 
-   int length;		// holds length
-   int width;		// holds width
-   int area;		// holds area
- 
-   int *lengthPtr;	// int pointer which will be set to point to length
-   int *widthPtr;   // int pointer which will be set to point to width
+	int length;		// holds length
+	int width;		// holds width
+	int area;		// holds area
 
-   cout << "Please input the length of the rectangle" << endl;
-   cin >> length;
-   cout << "Please input the width of the rectangle" << endl;
-   cin >> width;
+	int *lengthPtr;	// int pointer which will be set to point to length
+	int *widthPtr;   // int pointer which will be set to point to width
 
-   // Fill in code to make lengthPtr point to length (hold its address)
-   // Fill in code to make widthPtr point to width (hold its address) 
-   
+	cout << "Please input the length of the rectangle" << endl;
+	cin >> length;
+	cout << "Please input the width of the rectangle" << endl;
+	cin >> width;
 
-   area =  // Fill in code to find the area by using only the pointer variables
-   cout << "The area is " << area << endl;
+	/// Fill in code to make lengthPtr point to length (hold its address)
+	lengthPtr = &length;
+	/// Fill in code to make widthPtr point to width (hold its address) 
+	widthPtr = &width;
 
-
-
-   if (// Fill in the condition of length > width by using only the pointer variables)
-    	cout << "The length is greater than the width" << endl;
-
-   else if (// Fill in the condition of width > length by using only the pointer 
-	        // variables)
-    	cout << "The width is greater than the length" << endl;
-
-   else 
-    	cout << "The width and length are the same" << endl;
+	area = *lengthPtr * *widthPtr; /// Fill in code to find the area by using only the pointer variables
+	cout << "The area is " << area << endl;
 
 
-   return 0;
+
+	if (*lengthPtr > *widthPtr) /// Fill in the condition of length > width by using only the pointer variables)
+		cout << "The length is greater than the width" << endl;
+
+	else if (*lengthPtr < *widthPtr) /// Fill in the condition of width > length by using only the pointer 
+			 // variables)
+		cout << "The width is greater than the length" << endl;
+
+	else
+		cout << "The width and length are the same" << endl;
+
+
+	return 0;
 }
